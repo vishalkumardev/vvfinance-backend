b66e59f069139a6d4999fd58b10f0ec5a29829b1
5060vishalkumar <5060vishalkumar@gmail.com>
2024-11-01 15:00:44 +0530

    fix/message_response

--- module/loans/loans.controller.js
+++ module/loans/loans.controller.js
@@ -13,7 +13,7 @@ const getItemById = async (req, res) => {
     return res.status(200).json({
       success: true,
       data: { item },
-      message: "Item get successfully",
+      message: "Loan get successfully",
     });
   } catch (error) {
     return res.status(500).json({
@@ -35,7 +35,7 @@ const getAllItems = async (req, res) => {
     const items = await itemService.getAllItems(data);
     return res
       .status(200)
-      .json({ success: true, data: items, message: "Items get successfully" });
+      .json({ success: true, data: items, message: "Loans get successfully" });
   } catch (error) {
     return res.status(500).json({
       success: false,
@@ -61,7 +61,7 @@ const createItem = async (req, res) => {
 
     return res.status(201).json({
       success: true,
-      message: "Emi Created Successfully",
+      message: "File Upload Successfully",
     });
   } catch (error) {
     return res.status(500).json({
@@ -81,12 +81,12 @@ const updateItem = async (req, res) => {
     if (!item) {
       return res
         .status(404)
-        .json({ success: false, message: "Item not found" });
+        .json({ success: false, message: "Loan not found" });
     }
     return res.status(200).json({
       success: true,
       data: item,
-      message: "Item updated successfully",
+      message: "Loan updated successfully",
     });
   } catch (error) {
     return res.status(500).json({
@@ -105,11 +105,11 @@ const deleteItem = async (req, res) => {
     if (!item) {
       return res
         .status(404)
-        .json({ success: false, message: "Item not found" });
+        .json({ success: false, message: "Loan not found" });
     }
     return res.status(201).json({
       success: true,
-      message: "Item deleted successfully",
+      message: "Loan deleted successfully",
     });
   } catch (error) {
     return res.status(500).json({
@@ -128,15 +128,13 @@ const searchItems = async (req, res) => {
     const items = await itemService.searchItems(agreementId, user);
 
     if (!items) {
-      return res
-        .status(200)
-        .json({ success: false, message: "No items found" });
+      return res.status(200).json({ success: false, message: "No Loan found" });
     }
 
     return res.status(200).json({
       success: true,
       data: items,
-      message: "Items found successfully",
+      message: "Loan found successfully",
     });
   } catch (error) {
     return res.status(500).json({
